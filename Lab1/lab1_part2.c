a19ec5253c1b1acb2220fdec9f944fa86f4330e0
wkeitz <wkeitz@Weston>
2023-09-26 19:24:08 -0700

    Complete part2

--- Lab1/lab1_part2.c
+++ Lab1/lab1_part2.c
@@ -18,7 +18,13 @@ int main(){
 
     init(); 
 
-    char name_str[] = "NATHA"; //string
+    
+    
+    char name0[] = "Nathan";
+
+    char name1[] = "Weston";
+
+    char *name_str = name0; //string
     
     u08 len_name = strlen(name_str); //get string length
     
@@ -27,8 +33,27 @@ int main(){
     u08 x = 0; //check if zero or one index, screen col index
     
     u08 z = 1; 
+    
+    u08 choose_name = 0; 
 
     while(1){
+
+        if(get_btn() == 1){
+         
+            choose_name = choose_name ^ 1;
+
+            switch(choose_name){
+                case 0: 
+                    name_str = name0;
+                    break;
+                case 1:
+                    name_str = name1;
+                    break;
+            }
+
+            u08 len_name = strlen(name_str); //get string length
+
+        }
         
         if(x > 7 - len_name + 1){ //check if string will not fit on end of screen
   
@@ -41,6 +66,8 @@ int main(){
             print_string(name_str+((len_name-z)));
 
             z++;
+
+            x++; //increment column
 
             if(z == len_name){
            
@@ -56,14 +83,15 @@ int main(){
             lcd_cursor(x, 0); //set cursor 
 
             print_string(name_str); //print whole string
-
+            
+            x++; //increment column
         }
 
         _delay_ms(500); //delay half a second
 
         clear_screen(); //clear string
 
-        x++; //increment column
+        
         
         
     }
