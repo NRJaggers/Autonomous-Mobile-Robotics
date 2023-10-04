e595edfdcfe12376d281b5dea3dde90e03615448
Nathan Jaggers <nathanj8910@gmail.com>
2023-10-04 09:25:27 -0700

    adding functions file

--- Lab2/test.c
+++ Lab2/test.c
@@ -2,9 +2,12 @@
 #include <util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include "functions.h"
 
 int main(void) {
    init();  //initialize board hardware
+
+   test()
 
    return 0;
 }
