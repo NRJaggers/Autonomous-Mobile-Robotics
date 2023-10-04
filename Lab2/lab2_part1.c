6e38cb52dd02bd214119316b9d9a49698e697439
wkeitz <wrkeitz@gmail.com>
2023-10-04 09:02:52 -0700

    motor control

--- Lab2/lab2_part1.c
+++ Lab2/lab2_part1.c
@@ -15,11 +15,34 @@ Description: Design a motor function and a program with gradual acceleration,
 int main(){
 
     init();
+    
+    u08 state = 0;
 
     while(1){
-        
-        set_servo(0,127);
     
+        if(get_btn() == 1){
+
+            state = state ^ 1;
+
+            _delay_ms(100);
+
+        }
+
+        if(state){
+           
+            set_servo(0,137);
+
+            set_servo(1,117);
+        
+        }
+        
+        else{
+            
+            set_servo(0,127);
+
+            set_servo(1,127);
+        
+        }
     }
 
     return 0;
