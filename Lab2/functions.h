68fff3dbaff08d6c79d3df313ed767403abbcde6
Nathan Jaggers <nathanj8910@gmail.com>
2023-10-04 10:42:20 -0700

    adding ideas so i remember later

--- Lab2/functions.h
+++ Lab2/functions.h
@@ -7,4 +7,28 @@
 
 void test_include();
 
+/* ideas
+
+    make defines for left and right motors
+
+    # ignoring return values and parameters to pass in because I don't know
+    # what these funcions would need yet, these ideas are not fleshed out
+
+    motor_init() - initialize the motors to stop
+    ss_motor() - flip bit to start or stop motors 
+    spin() - set motors for bot to spin
+    forward() - make robot move forward. could make it so you can control speed
+    reverse() - same idea as above backwards
+    
+    turn()
+    u-turn()
+    # maybe the above two could be combined. mabe there would be a smart way to
+    # figure out what angle turn you would like to make and could implement that
+
+    # honestly may not end up using these functions, but it might be a good exercise?
+    # maybe it will make work down the line easier?
+*/
+
+
+
 #endif
