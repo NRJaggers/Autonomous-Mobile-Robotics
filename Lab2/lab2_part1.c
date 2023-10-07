846112326c9e037bc922859eba4f46375a86b7c4
Nathan Jaggers <nathanj8910@gmail.com>
2023-10-07 16:04:34 -0700

    part 1 needs test

--- Lab2/lab2_part1.c
+++ Lab2/lab2_part1.c
@@ -18,8 +18,10 @@ int main(){
     init();
     motor_init();
     
-    u08 state = 0;
-    u08 direction = 0;
+    u08 state = 0;  // could maybe combine two flags into 1, where 0 is off and 1-4 is
+                    // accelerate forward, decelerate forward, accelerate backward, decelerate backward
+    u08 direction = 1;
+    u08 speed = 0;
 
     while(1){
     
@@ -39,19 +41,48 @@ int main(){
            
             // use direction flag, go forward and increment. once full speed decrement
             // then switch direction and repeat process. might need another flag like accelerate
-            
-            motor(0,-50);
 
-            motor(1,-50);
+            //This switch could prob be reduced down to two states, I am leaving as is right now for
+            //development and readability purposes
+            switch(direction)
+            {
+                //accelerate forward
+                case 1:
+                    if(speed < 100)
+                        speed++;
+                    else
+                        direction = 2;
+                    break;
+                //deccelerate forward
+                case 2:
+                    if(speed > 0)
+                        speed--;
+                    else
+                        direction = 3;
+                    break;
 
-            _delay_ms(1000);
+                //accelerate reverse
+                case 3:
+                    if(speed > -100)
+                        speed--;
+                    else
+                        direction = 4;
+                    break;
+                //deccelerate reverse
+                case 4:
+                    if(speed < 0)
+                        speed++;
+                    else
+                        direction = 1;
+                    break;
+                
+                default:
+                    direction = 1;
+                    break;
+            }
 
-
-            motor(0,50);
-
-            motor(1,50);
-
-            _delay_ms(1000);
+            forward(speed);
+            _delay_ms(10);
 
         }
         
