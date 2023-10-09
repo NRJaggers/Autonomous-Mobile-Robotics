6d67ff6dadf0e4926daed827d535134844aaf208
wkeitz <wrkeitz@gmail.com>
2023-10-09 08:59:47 -0700

    lab2part2 description

---
+++ Lab2/lab2_part2.c
@@ -0,0 +1,11 @@
+/*
+Name: Nathan Jaggers and Weston Keitz  
+
+Assignment Number: Lab 2 Part 2 
+
+Description: Implement Braitenberg vehicles 2a (fear) and 2b (aggression).
+            Press button to toggle between modes. Display mode on LCD.
+            Stopped to full speed depending on amount of light recieved.
+
+*/
+
