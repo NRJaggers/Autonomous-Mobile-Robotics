ec017cc17fd04182263f21fdad1febc775b2b081
Nathan Jaggers <nathanj8910@gmail.com>
2023-10-09 08:59:28 -0700

    init lab 2 part 2

---
+++ Lab2/lab2_part2.c
@@ -0,0 +1,48 @@
+/*
+Name: Nathan Jaggers and Weston Keitz  
+
+Assignment Number: Lab 2 Part 2
+
+Description: 
+
+*/
+#include "globals.h"
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "functions.h"
+
+int main(){
+
+    init();
+    motor_init();
+    
+    u08 state = 1;
+    u08 direction = 1;
+    int8_t speed = 0;
+
+    while(1){
+    
+        if(get_btn() == 1){
+
+            state = state ^ 1;
+
+            _delay_ms(100);
+
+        }
+
+        if(state){
+
+            //start here
+
+
+        }
+        
+        else
+        {    
+            motor_init();
+        }
+    }
+
+    return 0;
+}
