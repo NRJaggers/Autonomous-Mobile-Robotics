598114201e463d8e8a51ba4f85bee308768bf222
wkeitz <wrkeitz@gmail.com>
2023-10-11 10:54:12 -0700

    part 4 update

--- Lab2/lab2_part4.c
+++ Lab2/lab2_part4.c
@@ -23,12 +23,13 @@ int main(){
     u08 left_sensor_value, right_sensor_value;
     int8_t error, diff;
     int8_t diff_past = 0;
+    int8_t speed;
 
-    const u08 Kp = 5;
+    const u08 Kp = 2;
     const u08 Kd = 1;
     motor(RIGHT, 10);
     motor(LEFT, 10);
-    _delay_ms(1000);
+   // _delay_ms(1000);
     while(1){
 
         
@@ -46,8 +47,16 @@ int main(){
         
         diff = (left_sensor_value - right_sensor_value);
         
-        error = (Kp * diff) - (Kd * (diff - diff_past)); // want error between 0 and 100
-
+        error = (Kp * diff) - (Kd * (diff - diff_past)); // want error between 0 and 10
+        if(error > 0){
+            speed = 10 - error; }
+        else{
+            speed = 10 + error;
+            }
+        
+        if (speed < 0){
+            speed = 0; }
+/*
         if((220 > left_sensor_value && left_sensor_value > 180) && 
             (220 > right_sensor_value && right_sensor_value > 180) && 
             (-10 < diff && diff < 10)){
@@ -55,17 +64,21 @@ int main(){
             motor(LEFT, 10);
             motor(RIGHT, 10);
         }
-        
+        */
         /*
         lcd_cursor(0,0);
         print_num(error);
         */
         else if(error > 0){
-            motor(RIGHT, 10 - error);
+            
+            motor(RIGHT, speed);
+            motor(LEFT, 10);
         }
 
         else{
-            motor(LEFT, 10 - error);
+            motor(LEFT, speed);
+            motor(LEFT, 10);
+
         }
 
         diff_past = diff; 
