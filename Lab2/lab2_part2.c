e8c94da3a341310e8caa1e03394fee3e89dfb5ff
wkeitz <wrkeitz@gmail.com>
2023-10-13 08:41:33 -0700

    part 2 and part 3

--- Lab2/lab2_part2.c
+++ Lab2/lab2_part2.c
@@ -17,37 +17,44 @@ Description: Implement Braitenberg vehicles 2a (fear) and 2b (aggression).
 
 int main(){
 
-    init();
+    init(); //initialize board
     
+    //save left/right sensor values and speed
     u08 left_sensor_value, right_sensor_value;
     u08 left_motor_speed, right_motor_speed;    
     
     u08 mode = FEAR; //0 is FEAR, 1 is AGGRESSION
-    clear_screen();
+    
+    //print initialized mode
+    clear_screen(); 
     print_string("FEAR");
     
     while(1){
 
-        if(get_btn() == 1){
+        if(get_btn() == 1){ //change modes on button press
             
-            mode = mode ^ 1;
+            mode = mode ^ 1; //toggle modes with XOR
             
             clear_screen();
             
+            //print mode on screen
             if(mode == FEAR){print_string("FEAR");}
+            
             else{print_string("AGR0");}
             
-            _delay_ms(200);
+            _delay_ms(200); //delay so button press works
 
         }
         
+        //read analog values
         left_sensor_value = analog(LEFT);
         right_sensor_value = analog(RIGHT);
        
-        // convert 0 - 255 to 0 - 100
+        // convert 0 - 255 to 0 - 100 (sensor value to speed)
         left_motor_speed = (100 * left_sensor_value) / 255;
         right_motor_speed = (100 * right_sensor_value) / 255;
         
+        //set motor speed, toggle side based on mode
         motor(LEFT ^ mode, left_motor_speed);
         motor(RIGHT ^ mode, right_motor_speed);
     
--- Lab2/lab2_part3.c
+++ Lab2/lab2_part3.c
@@ -17,37 +17,44 @@ Description: Implement Braitenberg vehicles 3a (attraction) and 3b (shy).
 
 int main(){
 
-    init();
-    
+    init(); //initialize board
+
+    //save left/right sensor values and speed  
     u08 left_sensor_value, right_sensor_value;
     u08 left_motor_speed, right_motor_speed;    
     
     u08 mode = ATTRACTION; //0 is ATTRACTION, 1 is SHY
+    
+    //print initialized mode
     clear_screen();
     print_string("ATTRACT");
     
     while(1){
 
-        if(get_btn() == 1){
+        if(get_btn() == 1){ //change modes on button press
             
             mode = mode ^ 1;
             
             clear_screen();
             
+            //print mode on screen
             if(mode == FEAR){print_string("ATTRACT");}
+            
             else{print_string("SHY");}
             
-            _delay_ms(200);
+            _delay_ms(200); //delay so button press works
 
         }
         
+        //read analog values
         left_sensor_value = analog(LEFT);
         right_sensor_value = analog(RIGHT);
        
-        // convert 0 - 255 to 0 - 100
+        // convert 0 - 255 to 100 - 0
         left_motor_speed = 100 - ((100 * left_sensor_value) / 255);
         right_motor_speed = 100 - ((100 * right_sensor_value) / 255);
         
+        //set motor speed, toggle side based on mode
         motor(LEFT ^ mode, left_motor_speed);
         motor(RIGHT ^ mode, right_motor_speed);
     
