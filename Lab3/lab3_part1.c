e63b91b6a9b42155735b8b7a2982ec700b8e6514
wkeitz <wrkeitz@gmail.com>
2023-10-18 09:37:06 -0700

    main file

---
+++ Lab3/lab3_part1.c
@@ -0,0 +1,15 @@
+/*
+Name: Nathan Jaggers and Weston Keitz  
+
+Assignment Number: Lab 3 Part 1 
+
+Description: 
+*/
+#include "globals.h"
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "functions.h"
+
+
+int main(){}
