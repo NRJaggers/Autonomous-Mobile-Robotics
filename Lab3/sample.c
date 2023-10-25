95c1dc7aa5fec8fef2e73c7dd4ac122bf22a4a75
Nathan Jaggers <nathanj8910@gmail.com>
2023-10-25 09:16:11 -0700

    Testing make file for pure c programs

--- Lab3/sample.c
+++ Lab3/sample.c
@@ -1,12 +1,15 @@
-#include "globals.h"
-#include <util/delay.h>
-#include <avr/io.h>
-#include <avr/interrupt.h>
+//#include "globals.h"
+//#include <util/delay.h>
+// #include <avr/io.h>
+// #include <avr/interrupt.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-   init();  //initialize board hardware
+   //init();  //initialize board hardware
 
-   print_string("Hello");
+   //print_string("Hello");
+   printf("hello world\n");
 
    return 0;
 }
