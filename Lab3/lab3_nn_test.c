a41d82c052ffeac7a8e506ee5b41144c39484fde
wkeitz <wrkeitz@gmail.com>
2023-10-27 09:03:59 -0700

    working version 1

--- Lab3/lab3_nn_test.c
+++ Lab3/lab3_nn_test.c
@@ -19,13 +19,13 @@ maybe try divide target with 100 and compute with that.
 
 #define BASE_SPEED 30 //cruising speed for bot
 #define ERROR_THRESH 5 // Threshold for error between sensors before control activates
-#define BIAS_CONST 1
+#define BIAS_CONST -1/255
 #define SCALE 10
 #define PERCENT 100
 #define SENSOR_MAX 255
 
 #define EPOCHS 100
-#define DATA_POINTS 20  // more than 50 seems to lead to memeory problems; only 4k for variables
+#define DATA_POINTS 100  // more than 50 seems to lead to memeory problems; only 4k for variables
 #define PARAMS 17       //hidden layer (2 input + bias)* 3 nodes + (3 input + bias) * 2 nodes
 #define ALPHA 0.015
 
@@ -34,8 +34,9 @@ float sigmoid(double x){
 };
 
 float d_sigmoid(float s){
-   // double s = sigmoid(x);
+  //  float x = sigmoid(s);
     //Assuming input coming in is already sigmoided
+   // return x* (1 - x);
     return s* (1 - s);
 };
 
@@ -101,13 +102,13 @@ struct MotorValues compute_neural_network(float left_sensor, float right_sensor,
     float right_scaled = right_sensor / SENSOR_MAX;
     struct MotorValues m1;
 
-    m1.h1 = sigmoid((d1.parameters[0] * left_scaled) + (d1.parameters[1] * right_scaled) + d1.parameters[2] * BIAS_CONST);
-    m1.h2 = sigmoid((d1.parameters[3] * left_scaled) + (d1.parameters[4] * right_scaled) + d1.parameters[5] * BIAS_CONST);
-    m1.h3 = sigmoid((d1.parameters[6] * left_scaled) + (d1.parameters[7] * right_scaled) + d1.parameters[8] * BIAS_CONST);
+    m1.h1 = sigmoid((d1.parameters[0] * left_scaled) + (d1.parameters[1] * right_scaled) + (d1.parameters[2] * BIAS_CONST));
+    m1.h2 = sigmoid((d1.parameters[3] * left_scaled) + (d1.parameters[4] * right_scaled) + (d1.parameters[5] * BIAS_CONST));
+    m1.h3 = sigmoid((d1.parameters[6] * left_scaled) + (d1.parameters[7] * right_scaled) + (d1.parameters[8] * BIAS_CONST));
 
     //sigmoid results in value from 0-1.0, therefore multiply by 100
-    m1.left = sigmoid((d1.parameters[9] * m1.h1) + (d1.parameters[10] * m1.h2) + (d1.parameters[11] * m1.h3) + d1.parameters[12] * BIAS_CONST);
-    m1.right = sigmoid((d1.parameters[13] * m1.h1) + (d1.parameters[14] * m1.h2) + (d1.parameters[15] * m1.h3) + d1.parameters[16] * BIAS_CONST);
+    m1.left = sigmoid((d1.parameters[9] * m1.h1) + (d1.parameters[10] * m1.h2) + (d1.parameters[11] * m1.h3) + (d1.parameters[12] * BIAS_CONST));
+    m1.right = sigmoid((d1.parameters[13] * m1.h1) + (d1.parameters[14] * m1.h2) + (d1.parameters[15] * m1.h3) + (d1.parameters[16] * BIAS_CONST));
 
     return m1;
 }
@@ -177,37 +178,33 @@ struct NeuralData train_neural_network(int epochs_max, float alpha,  struct Neur
             float h3Temp = d_sigmoid(mV.h3);
 
             //update w1
-            dE[0] = ((c1Temp*nD.parameters[10-1]) + (c2Temp*nD.parameters[14-1])) * h1Temp * (nD.left_sensor_values[i]/SENSOR_MAX);
+            dE[0] = ((c1Temp*nD.parameters[9]) + (c2Temp*nD.parameters[13])) * h1Temp * (nD.left_sensor_values[i]/SENSOR_MAX);
             //update w2
-            dE[1] = ((c1Temp*nD.parameters[10-1]) + (c2Temp*nD.parameters[14-1])) * h1Temp * (nD.right_sensor_values[i]/SENSOR_MAX);
+            dE[1] = ((c1Temp*nD.parameters[9]) + (c2Temp*nD.parameters[13])) * h1Temp * (nD.right_sensor_values[i]/SENSOR_MAX);
             //update w3
-            dE[2] = ((c1Temp*nD.parameters[10-1]) + (c2Temp*nD.parameters[14-1])) * h1Temp * BIAS_CONST;
+            dE[2] = ((c1Temp*nD.parameters[9]) + (c2Temp*nD.parameters[13])) * h1Temp * BIAS_CONST;
 
             //update w4
-            dE[3] = ((c1Temp*nD.parameters[11-1]) + (c2Temp*nD.parameters[15-1])) * h2Temp * (nD.left_sensor_values[i]/SENSOR_MAX);
+            dE[3] = ((c1Temp*nD.parameters[10]) + (c2Temp*nD.parameters[13])) * h2Temp * (nD.left_sensor_values[i]/SENSOR_MAX);
             //update w5
-            dE[4] = ((c1Temp*nD.parameters[11-1]) + (c2Temp*nD.parameters[15-1])) * h2Temp * (nD.right_sensor_values[i]/SENSOR_MAX);
+            dE[4] = ((c1Temp*nD.parameters[10]) + (c2Temp*nD.parameters[13])) * h2Temp * (nD.right_sensor_values[i]/SENSOR_MAX);
             //update w6
-            dE[5] = ((c1Temp*nD.parameters[11-1]) + (c2Temp*nD.parameters[15-1])) * h2Temp * BIAS_CONST;
+            dE[5] = ((c1Temp*nD.parameters[10]) + (c2Temp*nD.parameters[13])) * h2Temp * BIAS_CONST;
 
             //update w7
-            dE[6] = ((c1Temp*nD.parameters[12-1]) + (c2Temp*nD.parameters[16-1])) * h3Temp * (nD.left_sensor_values[i]/SENSOR_MAX);
+            dE[6] = ((c1Temp*nD.parameters[11]) + (c2Temp*nD.parameters[15])) * h3Temp * (nD.left_sensor_values[i]/SENSOR_MAX);
             //update w8
-            dE[7] = ((c1Temp*nD.parameters[12-1]) + (c2Temp*nD.parameters[16-1])) * h3Temp * (nD.right_sensor_values[i]/SENSOR_MAX);
+            dE[7] = ((c1Temp*nD.parameters[11]) + (c2Temp*nD.parameters[15])) * h3Temp * (nD.right_sensor_values[i]/SENSOR_MAX);
             //update w9
-            dE[8] = ((c1Temp*nD.parameters[12-1]) + (c2Temp*nD.parameters[16-1])) * h3Temp * BIAS_CONST;
+            dE[8] = ((c1Temp*nD.parameters[11]) + (c2Temp*nD.parameters[15])) * h3Temp * BIAS_CONST;
 
             for(int j = 0 ; j < PARAMS; j++){
                 nD.parameters[j] = nD.parameters[j] - (alpha * dE[j]);
             }
-            
+
         } 
         
-        printf("---- Epcoh %d ----\n", epochs);
-
-        printf("NN Hidden Output 1:%.3f \n",mV.h1);
-        printf("NN Hidden Output 2:%.3f \n",mV.h2);
-        printf("NN Hidden Output 3:%.3f \n",mV.h2);
+        
     
         for(int i = 0; i< PARAMS; i++){printf("NN Parameter %d:%.3f \n",i,nD.parameters[i]);}
 
@@ -247,66 +244,11 @@ int main(){
     struct MotorValues m;    
     struct motor_command t;
 
-    trainingData.left_sensor_values[0] = 110;
-    trainingData.right_sensor_values[0] = 101;
-
-    trainingData.left_sensor_values[1] = 130;
-    trainingData.right_sensor_values[1] = 124;
-
-    trainingData.left_sensor_values[2] = 196;
-    trainingData.right_sensor_values[2] = 118;
-
-    trainingData.left_sensor_values[3] = 192;
-    trainingData.right_sensor_values[3] = 102;
-
-    trainingData.left_sensor_values[4] = 157;
-    trainingData.right_sensor_values[4] = 93;
-
-    trainingData.left_sensor_values[5] = 101;
-    trainingData.right_sensor_values[5] = 163;
-
-    trainingData.left_sensor_values[6] = 117;
-    trainingData.right_sensor_values[6] = 111;
-
-    trainingData.left_sensor_values[7] = 100;
-    trainingData.right_sensor_values[7] = 192;
-
-    trainingData.left_sensor_values[8] = 168;
-    trainingData.right_sensor_values[8] = 77;
-
-    trainingData.left_sensor_values[9] = 192;
-    trainingData.right_sensor_values[9] = 55;
-
-    trainingData.left_sensor_values[10] = 118;
-    trainingData.right_sensor_values[10] = 196;
-
-    trainingData.left_sensor_values[11] = 108;
-    trainingData.right_sensor_values[11] = 116;
-
-    trainingData.left_sensor_values[12] = 103;
-    trainingData.right_sensor_values[12] = 94;
-
-    trainingData.left_sensor_values[13] = 192;
-    trainingData.right_sensor_values[13] = 194;
-
-    trainingData.left_sensor_values[14] = 105;
-    trainingData.right_sensor_values[14] = 111;
-
-    trainingData.left_sensor_values[15] = 188;
-    trainingData.right_sensor_values[15] = 77;
-
-    trainingData.left_sensor_values[16] = 98;
-    trainingData.right_sensor_values[16] = 92;
-
-    trainingData.left_sensor_values[17] = 99;
-    trainingData.right_sensor_values[17] = 195;
-
-    trainingData.left_sensor_values[18] = 111;
-    trainingData.right_sensor_values[18] = 109;
-
-    trainingData.left_sensor_values[19] = 100;
-    trainingData.right_sensor_values[19] = 91;
-
+    for(int i = 0 ; i < DATA_POINTS;i++){
+        trainingData.left_sensor_values[i] = 255*(float)rand() / RAND_MAX;
+        trainingData.right_sensor_values[i] = 255*(float)rand() / RAND_MAX;
+    }
+  
     for(int i = 0; i < PARAMS; i++){
         trainingData.parameters[i] = (float)rand() / RAND_MAX;
         // printf("%1.4f \n", trainingData.parameters[i]);
@@ -317,7 +259,7 @@ int main(){
     //     printf("Param %d: %2.6f\n",k,trainingData.parameters[k]);
     // }
     
-    trainingData = train_neural_network(20, 0.01, trainingData);
+    trainingData = train_neural_network(1000, 0.01, trainingData);
     
     // printf("---Trained Param---\n");
     // for(int j = 0 ; j < PARAMS; j++){
@@ -327,20 +269,85 @@ int main(){
     for(int j = 0 ; j < DATA_POINTS; j++){
         m = compute_neural_network(trainingData.left_sensor_values[j],trainingData.right_sensor_values[j], trainingData);
         t = compute_proportional(trainingData.left_sensor_values[j],trainingData.right_sensor_values[j]);
-    
+
+        
         printf("---- Data Point %d ----\n", j);
+        // printf("NN Hidden Output 1:%.3f \n",m.h1);
+        // printf("NN Hidden Output 2:%.3f \n",m.h2);
+        // printf("NN Hidden Output 3:%.3f \n",m.h3);
         // printf("Hidden: %0.4f\n",trainingData.parameters[j]);
-        printf("Inputs     L:%1.3f R%1.3f \n", trainingData.left_sensor_values[j],trainingData.right_sensor_values[j]);
+     //   printf("Inputs     L:%1.3f R%1.3f \n", trainingData.left_sensor_values[j],trainingData.right_sensor_values[j]);
         // printf("%f\n", t.right_motor);
         printf("NN Outputs L:%2.3f R%2.3f \n", m.left*100,m.right*100);
         printf("PP Outputs L:%2.3f R%2.3f \n", t.left_motor,t.right_motor);
     }
-    for(int i = 0; i< PARAMS; i++){printf("NN Parameter:%.3f \n",trainingData.parameters[i]);}
+   // for(int i = 0; i< PARAMS; i++){printf("NN Parameter:%.3f \n",trainingData.parameters[i]);}
 
     printf("Done\n");           
     return 0;
 
 }
+
+
+
+
+
+  // trainingData.left_sensor_values[1] = 130;
+    // trainingData.right_sensor_values[1] = 124;
+
+    // trainingData.left_sensor_values[2] = 196;
+    // trainingData.right_sensor_values[2] = 118;
+
+    // trainingData.left_sensor_values[3] = 192;
+    // trainingData.right_sensor_values[3] = 102;
+
+    // trainingData.left_sensor_values[4] = 157;
+    // trainingData.right_sensor_values[4] = 93;
+
+    // trainingData.left_sensor_values[5] = 101;
+    // trainingData.right_sensor_values[5] = 163;
+
+    // trainingData.left_sensor_values[6] = 117;
+    // trainingData.right_sensor_values[6] = 111;
+
+    // trainingData.left_sensor_values[7] = 100;
+    // trainingData.right_sensor_values[7] = 192;
+
+    // trainingData.left_sensor_values[8] = 168;
+    // trainingData.right_sensor_values[8] = 77;
+
+    // trainingData.left_sensor_values[9] = 192;
+    // trainingData.right_sensor_values[9] = 55;
+
+    // trainingData.left_sensor_values[10] = 118;
+    // trainingData.right_sensor_values[10] = 196;
+
+    // trainingData.left_sensor_values[11] = 108;
+    // trainingData.right_sensor_values[11] = 116;
+
+    // trainingData.left_sensor_values[12] = 103;
+    // trainingData.right_sensor_values[12] = 94;
+
+    // trainingData.left_sensor_values[13] = 192;
+    // trainingData.right_sensor_values[13] = 194;
+
+    // trainingData.left_sensor_values[14] = 105;
+    // trainingData.right_sensor_values[14] = 111;
+
+    // trainingData.left_sensor_values[15] = 188;
+    // trainingData.right_sensor_values[15] = 77;
+
+    // trainingData.left_sensor_values[16] = 98;
+    // trainingData.right_sensor_values[16] = 92;
+
+    // trainingData.left_sensor_values[17] = 99;
+    // trainingData.right_sensor_values[17] = 195;
+
+    // trainingData.left_sensor_values[18] = 111;
+    // trainingData.right_sensor_values[18] = 109;
+
+    // trainingData.left_sensor_values[19] = 100;
+    // trainingData.right_sensor_values[19] = 91;
 
 
 
