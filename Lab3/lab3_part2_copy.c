8291c13fba04587d1e9c7fd8eb59e21291674dec
wkeitz <wrkeitz@gmail.com>
2023-10-30 08:55:41 -0700

    comments

--- Lab3/lab3_part2_copy.c
+++ Lab3/lab3_part2_copy.c
@@ -27,34 +27,36 @@ int main(){
     state_Robot state = STD_MODE;
 
     u08 left_sensor_value, right_sensor_value; //read analog sensor values
-    struct motor_command speed;
-    struct NeuralData trainingData;
-    struct MotorValues m_speed;
-    
-    int index = 0;
-    int epochs; 
- //   u08 data_gathered = 0;
-    float x;
+    struct motor_command speed; // proportional controller output values
+    struct NeuralData trainingData; // sensor values for NN training
+    struct MotorValues m_speed; //  compute NN output values
+
+    int index = 0; // training data index
+    int epochs; // epochs count for training
+    float x; // accelerometer sensor value
   
-    float parameters[PARAMS];
+    float parameters[PARAMS]; // Weights for computation and training
 
     while(1){
-        //read and print sensor values
-        // left_sensor_value = analog(ANALOG4_PIN); 
-        // right_sensor_value = analog(ANALOG3_PIN);
 
-        switch(state){
+        switch(state){ // state machine for controlling modes
 
             case STD_MODE:
+                // print proportional mode
                 clear_screen();
                 lcd_cursor(0,0);
                 print_string("Propor-");
                 lcd_cursor(0,1);
                 print_string("tional");
+                _delay_ms(1000);
 
+                //stay in propotion mode
                 while(!get_btn()){
+                    //read sensor values
                     left_sensor_value = analog(ANALOG4_PIN); 
                     right_sensor_value = analog(ANALOG3_PIN);
+
+                    //print sensor values (for debugging)
                     clear_screen();
                     lcd_cursor(0,0);
                     print_string("Propor-");
@@ -62,14 +64,17 @@ int main(){
                     print_num(left_sensor_value);
                     lcd_cursor(5,1);
                     print_num(right_sensor_value);
-                    _delay_ms(15);
+                    _delay_ms(15); //stops flickering
+
+                    //compute proporitional correction and set motors
                     speed = compute_proportional(left_sensor_value, right_sensor_value);
                     motor(LEFT, speed.left_motor);
                     motor(RIGHT, speed.right_motor);
 
+                    //change mode to NN data collection if button pressed
                     if(get_btn()){
                         state = DATA_MODE;
-                        motor_init();
+                        motor_init(); //stop motors
                         _delay_ms(BTN_DELAY);
                         break;
                     }
@@ -78,112 +83,91 @@ int main(){
                 break;
 
             case DATA_MODE:
+                //print data mode
                 clear_screen();
                 lcd_cursor(0,0);
                 print_string("Data ");
-              //  (!data_gathered) ? print_num(index) : print_num(DATA_POINTS);
+
+                //read sensor values
                 left_sensor_value = analog(ANALOG4_PIN); 
-                right_sensor_value = analog(ANALOG3_PIN);
-                
-                
-                // speed = compute_proportional(left_sensor_value, right_sensor_value);
-               
-                // if(index >= DATA_POINTS - 1){
-                //     index = 0;
-                //     data_gathered = 1; 
-                // }
+                right_sensor_value = analog(ANALOG3_PIN);            
 
-                //save proportional data and increment index
-               
-
-                // trainingData.left_sensor_values[index] = 255* (rand() / RAND_MAX);
-                // trainingData.right_sensor_values[index] = 255* (rand() / RAND_MAX);
-
-                
-
-                // trainingData.left_motor_values[index] = speed.left_motor;
-                // trainingData.right_motor_values[index] = speed.right_motor;
-
-                
-
+                //collect data until max
                 if(index >= DATA_POINTS){
-                    // for(int i = 0; i < PARAMS; i++){
-                    //     parameters[i] = (float)rand() / RAND_MAX;
-                    // }
-                    //clear_screen();
-                    //print_num((u16) (trainingData.parameters[10] * 1000));
+                    //print data index max
                     print_num(DATA_POINTS);
                     
+                    //change state to epoch choice if button press
                     if(get_btn()){
                         state = CHOOSE_EPOCHS;
                         _delay_ms(BTN_DELAY);
                     }
                 }
                 else{
+                    // print index and store sensor values
                     print_num(index);
                     trainingData.left_sensor_values[index] = left_sensor_value;
                     trainingData.right_sensor_values[index] = right_sensor_value;
 
                 }
-               
+
+                //print sensor values on screen (for debugging)
                 lcd_cursor(0,1);
                 print_num(left_sensor_value);
-
                 lcd_cursor(5,1);
                 print_num(right_sensor_value);
 
+                //incrment index
                 index++;
-
-                _delay_ms(100); //10 total seconds of data gathering time
-                
-                
+                _delay_ms(100); //delay for human movement factor
                 break;
 
             case CHOOSE_EPOCHS:
-            
-                
-                
-                x = get_accel_x(); // get x-axis
 
-                if(100 > x && x > 0){epochs = floor(50 *x + 500);} // set top row
-                if(100 > x && x > 0){epochs = floor(50 *x + 1500)/SCALE;} // set top row
-        
-                else if(255 > x && x > 190){epochs = floor((500 * (x - 190)) / 65) + 0;} //set bottom row screen
-                else if(255 > x && x > 190){epochs = (floor((500 * (x - 190)) / 65) + 1000)/SCALE;} //set bottom row screen
+                //read accelerometer value
+                x = get_accel_x(); // get x-axis
                 
+                // convert accelerometer value to epoch count
+                if(100 > x && x > 0){epochs = floor(50 *x + 1500)/SCALE;} 
+                else if(255 > x && x > 190){epochs = (floor((500 * (x - 190)) / 65) + 1000)/SCALE;}
+                
+                // print training epoch count
                 clear_screen();
                 lcd_cursor(0,0);
                 print_string("Epochs: ");
                 lcd_cursor(0,1);
                 print_num(epochs);
-                _delay_ms(50); //flickering
+                _delay_ms(15); //flickering
+                
+                //if button press, enter training mode
                 if(get_btn()){
                     state = TRAIN_MODE;
                     _delay_ms(BTN_DELAY);
                 }
-                
                 break;
+
             case TRAIN_MODE:
-              //  motor_init();
                 
+                // print training message
+                clear_screen();
+                lcd_cursor(0,0);
+                print_string("Training");
 
-             //   if(get_btn()){
-                    clear_screen();
-                    lcd_cursor(0,0);
-                    print_string("Training");
-                                        
-                    train_neural_network(epochs, ALPHA, &trainingData, parameters);
-                    
-                    lcd_cursor(0,1);
-                    print_string("Done!");
-                    _delay_ms(BTN_DELAY);
+                //training function, pass epochs, learning rate, training data, and parameters           
+                train_neural_network(epochs, ALPHA, &trainingData, parameters);
+                
+                //print done when complete
+                lcd_cursor(0,1);
+                print_string("Done!");
+                _delay_ms(BTN_DELAY);
 
-                    state = NN_MODE;
-             //   }
-        
+                //enter
+                state = NN_MODE;
+    
                 break;
                 
             case NN_MODE:
+
                 clear_screen();
                 motor_init();
                
@@ -211,9 +195,7 @@ int main(){
                         motor_init();
 
                         index = 0;
-                        // for(int i = 0; i < PARAMS; i++){
-                        //     parameters[i] = (float)rand() / RAND_MAX;
-                        // }
+
 
                         state = CHOOSE_EPOCHS;
 
@@ -221,7 +203,6 @@ int main(){
                         break;
                     }
                 }
-                // break;
         }
     }
     return 0;
