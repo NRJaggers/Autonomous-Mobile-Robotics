3923326d2f21598c59eaf82a8c5480f1913c055c
wkeitz <wrkeitz@gmail.com>
2023-11-04 11:54:19 -0700

    Update lab4_part2.c

--- Lab4/lab4_part2.c
+++ Lab4/lab4_part2.c
@@ -15,7 +15,41 @@ Description:
 #include <math.h>
 #include <stdlib.h> 
 
+#define PI 3.141592654
 
+
+//add = 0 and scale = 1 for normal distribution
+float gaussian_sample(float shift, float scale){
+    
+    float u1 = (float) rand() / RAND_MAX;
+    
+    float u2 = (float) rand() / RAND_MAX;
+
+    float z = sqrtf(-2 * logf(u1)) * cosf(2 * PI * u2);
+
+    return (z * scale) + shift;
+
+}
+
+float prob_tower_given_sensor(float sensor_val, float a, float b, float c, float d){
+    
+    float u = 2 /(c + d - a - b);
+    
+    if(a <= x && x < b){return u * (sensor_val - a) / (b - a);}
+    
+    else if(b <= x && x < c){return u;}
+
+    else if(c <= x && x < d){return u * (d - sensor_val) / (c - d);}
+
+    else{return 0;}
+
+}
+
+float prob_free_space_given sensor(float sensor_val, float a, float b, float c, float d){
+    
+    return 1 - prob_tower_given_sensor(sensor_val, a, b, c, d);
+    
+}
 
 int main(){
 
