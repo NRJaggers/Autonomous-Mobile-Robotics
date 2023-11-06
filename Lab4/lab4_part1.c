444c9533e821bc8fb2eabd8262cd71d9808b24c7
Nathan Jaggers <nathanj8910@gmail.com>
2023-11-05 18:06:31 -0800

    comments and adjustments to part 1

--- Lab4/lab4_part1.c
+++ Lab4/lab4_part1.c
@@ -38,6 +38,9 @@ Notes:  Need to tape encoder disk on wheel
         Speed:      Time Count:       Ticks:      Loops:
         #           #                 #           #
         
+        maybe add more code here to input loops taken and have another state
+        that goes around circle and shows ticks, calculated robots current 
+        angle (assume starting at 0) and maybe range finder reading  
 */
 
 #include "globals.h"
@@ -97,11 +100,13 @@ int main(){
     init_encoder(); //initialize encoder
 
     // state machine modes
-    typedef enum {INIT, PROP, STOP} state_Robot;
+    typedef enum {INIT, PROP, STOP, DEGR, STAT} state_Robot;
     state_Robot state = INIT;
 
     u08 left_sensor_value, right_sensor_value; //read analog sensor values
     u16 time_count, time_count_max = COUNTMAX;
+    u16 degree = 0;
+    float loops = 0, ratio = 1;
     struct motor_command speed; // proportional controller output values
 
     while(TRUE)
@@ -173,11 +178,82 @@ int main(){
                 
                 break;
 
-            case STOP:
+            case STOP: //add functionality to choose next state
                 //Stop and hold state to observe recorded values
                 lcd_cursor(0,0);
                 print_string("STOP:");
                 print_num(time_count);
+                lcd_cursor(0,1);
+                print_num(left_encoder); //Digital input 4
+                lcd_cursor(5,1);
+                print_num(right_encoder); //Digital input 5
+
+                //change mode to INIT if pressed
+                if(get_btn()){
+                    state = INIT;
+                    _delay_ms(BTN_DELAY);
+
+                    //change to choose next state
+
+                    //then if state is DEGR, choose how many loops completed
+                }
+
+                break;
+
+            case DEGR:
+                //Wait untill button press (put robot into position)
+                while(!get_btn()){/*wait*/};
+                _delay_ms(BTN_DELAY);
+                degree = 0;
+                time_count = 0;
+                clear_screen();
+
+                //Start Robot at degree 0 in circle and let it run around
+                //Here you can test for accuracy in tick to degree
+                while(state == DEGR)
+                {
+                    //read sensor values
+                    left_sensor_value = analog(ANALOG4_PIN); 
+                    right_sensor_value = analog(ANALOG3_PIN);
+
+                    //print state and encoder values
+                    lcd_cursor(0,0);
+                    print_string("DEGR:");
+                    print_num(degree);
+                    lcd_cursor(0,1);
+                    print_num(left_encoder); //Digital input 4
+                    lcd_cursor(5,1);
+                    print_num(right_encoder); //Digital input 5
+                    //_delay_ms(15); // stops flickering (don't know that we need this
+                                // if we are not clearing screen)
+
+                    //compute proporitional correction and set motors
+                    speed = compute_proportional(left_sensor_value, right_sensor_value);
+                    motor(LEFT, speed.left_motor);
+                    motor(RIGHT, speed.right_motor);
+
+                    //calculate degrees from ticks
+                    degree = (float)left_encoder * ratio;
+
+                    //change mode to STOP if button pressed or if time limit reached
+                    //(limit should be associated with how long till x loops
+                    // around the circle) 
+                    if((get_btn())|(time_count >= time_count_max)){
+                        state = STAT;
+                        motor_init(); //stop motors
+                        _delay_ms(BTN_DELAY);
+                    }
+
+                    time_count++;
+                }
+
+                break;
+
+                case STAT: //add functionality to choose next state
+                //Stop and hold state to observe recorded values
+                lcd_cursor(0,0);
+                print_string("STAT:");
+                print_num(degree);
                 lcd_cursor(0,1);
                 print_num(left_encoder); //Digital input 4
                 lcd_cursor(5,1);
--- Lab4/lab4_part2.c
+++ Lab4/lab4_part2.c
@@ -5,6 +5,22 @@ Assignment Number: Lab 4 Part 2
 
 Description:
 
+Notes:  make function to calculate std_dev of particles? 
+        then function for mean which represents location?
+        
+        make MCL a function?
+            - generate n particles
+            - take 1 sensor reading
+            - for each particle:
+                ~ advance the particle (and add noise)
+                ~ categorize particle (tower or free space)
+                ~ compute weight for particle
+            - resample particles
+
+Questions:  Should probablility of tower and free space functions add to 1?
+            How are a,b,c,d determined? Experimentally with sensor and hardcode?
+
+
 */
 
 #include "globals.h"
@@ -18,7 +34,7 @@ Description:
 #define PI 3.141592654
 
 
-//add = 0 and scale = 1 for normal distribution
+//Box-Muller Transform: function to create sample from gaussian curve
 float gaussian_sample(float shift, float scale){
     
     float u1 = (float) rand() / RAND_MAX;
@@ -27,27 +43,34 @@ float gaussian_sample(float shift, float scale){
 
     float z = sqrtf(-2 * logf(u1)) * cosf(2 * PI * u2);
 
+    //add shift = 0 and multiply scale = 1 for normal distribution
     return (z * scale) + shift;
 
 }
 
+//function for calculating probablilty of getting sensor_val given that the 
+//robot/particle position is at a tower 
 float prob_tower_given_sensor(float sensor_val, float a, float b, float c, float d){
     
     float u = 2 /(c + d - a - b);
     
-    if(a <= x && x < b){return u * (sensor_val - a) / (b - a);}
+    if(a <= sensor_val && sensor_val < b){return u * (sensor_val - a) / (b - a);}
     
-    else if(b <= x && x < c){return u;}
+    else if(b <= sensor_val && sensor_val < c){return u;}
 
-    else if(c <= x && x < d){return u * (d - sensor_val) / (c - d);}
+    else if(c <= sensor_val && sensor_val < d){return u * (d - sensor_val) / (c - d);}
 
     else{return 0;}
 
 }
 
+//function for calculating probablilty of getting sensor_val given that the 
+//robot/particle position is at a tower
 float prob_free_space_given sensor(float sensor_val, float a, float b, float c, float d){
     
-    return 1 - prob_tower_given_sensor(sensor_val, a, b, c, d);
+    return 1 - prob_tower_given_sensor(sensor_val, a, b, c, d); //yeah I see what you mean by your question now
+    //we should ask him in class if they add up to 1 or if we are supposed to have independent
+    //functions and curves.
     
 }
 
