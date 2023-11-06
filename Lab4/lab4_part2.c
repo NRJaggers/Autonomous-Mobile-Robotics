86d48a6e946e66642e34a79850571e6acc56600b
wkeitz <wrkeitz@gmail.com>
2023-11-05 16:33:40 -0800

    update

--- Lab4/lab4_part2.c
+++ Lab4/lab4_part2.c
@@ -16,7 +16,8 @@ Description:
 #include <stdlib.h> 
 
 #define PI 3.141592654
-
+#define PARTICLE_COUNT 100
+#define NUM_TOWERS 3 
 
 //add = 0 and scale = 1 for normal distribution
 float gaussian_sample(float shift, float scale){
@@ -31,7 +32,8 @@ float gaussian_sample(float shift, float scale){
 
 }
 
-float prob_tower_given_sensor(float sensor_val, float a, float b, float c, float d){
+
+float prob_tower_given_location(float sensor_val, float a, float b, float c, float d){
     
     float u = 2 /(c + d - a - b);
     
@@ -45,10 +47,26 @@ float prob_tower_given_sensor(float sensor_val, float a, float b, float c, float
 
 }
 
-float prob_free_space_given sensor(float sensor_val, float a, float b, float c, float d){
+float prob_free_space_given_location(float sensor_val, float a, float b, float c, float d){
     
-    return 1 - prob_tower_given_sensor(sensor_val, a, b, c, d);
+    return 1 - prob_tower_given_location(sensor_val, a, b, c, d);
     
+}
+
+//complete this function
+u08 read_ir(void){
+    return sensor;
+}
+
+struct towers{
+    float location[NUM_TOWERS];
+    u08 target;
+}
+
+float prob_tower_given_location(float angle0, float angle1, float angle2){
+
+    
+
 }
 
 int main(){
@@ -56,7 +74,29 @@ int main(){
     init(); //initialize board
     motor_init(); //initialize motors 
 
+    u08 ir_value; 
+    //initialize particle position
+    float particles[PARTICLE_COUNT];
+
+    for(int i = 0; i < PARTICLE_COUNT; i++){
+        particles[i] = 360 * (float) rand() / RAND_MAX;
+    }
+
+    //create map
+    towers.location[0] = 0; 
+    towers.location[0] = 90; 
+    towers.location[0] = 225; 
+    towers.target = 1; 
+
     while(1){
+
+        ir_value = read_ir();
+
+        for(int i = 0; i < PARTICLE_COUNT; i++){
+            prob_tower_given_location(particle[i], float a, float b, float c, float d)
+        }
+        
+
         
     }
     
