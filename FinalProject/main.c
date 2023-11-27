ff7ac21502724cfeb325ef17bbbb6ccd20d7720e
wkeitz <wrkeitz@gmail.com>
2023-11-27 09:26:00 -0800

    update

--- FinalProject/main.c
+++ FinalProject/main.c
@@ -14,9 +14,76 @@ the area as well.
 #include <avr/interrupt.h>
 #include "functions.h"
 
-int main(){
+#define RANGE_UPPER_THRESHOLD 200
+#define RANGE_LOWER_THRESHOLD 100
 
-    int range = 
+int search_for_cans(){
+
+}
+int main(){
+    int range; 
+
+    typedef enum {READ_SENSORS, MOVEMENT} state_Robot;
+
+    state_Robot state = READ_SENSORS;
+
+    while(1){
+        
+        switch(state){
+
+            case READ_SENSORS:
+                range = read_range_finder();
+
+                while(RANGE_LOWER_THRESHOLD < range  && range < RANGE_UPPER_THRESHOLD){
+                    
+                    for(int i = 0; i < 30){
+                        turn_right_degrees(1);
+                        if(LEFT || RIGHT SENSOR == BLACK)
+                            //force a correction
+                            //break both loops
+                        range = read_range_finder();
+                        if(RANGE_LOWER_THRESHOLD < range  && range < RANGE_UPPER_THRESHOLD){
+                            state = MOVEMENT;
+                            break; // change state
+                        }
+                    }
+
+                    turn_left_degrees(30);
+
+                    for(int i = 0; i < 30){
+                        turn_left_degrees(1);
+                        if(LEFT || RIGHT SENSOR == BLACK)
+                           //force a correction
+                           //break loops
+
+                        range = read_range_finder();
+                        if(RANGE_LOWER_THRESHOLD < range  && range < RANGE_UPPER_THRESHOLD){
+                            state = MOVEMENT;
+                            break; // change state
+                        }
+                    }
+
+                    //randomly readjust and move
+                    turn_right_degrees(random value);
+                   
+                    move(); // a few units
+                    
+                    
+                }
+
+                break;
+
+            case MOVEMENT:
+
+                break;
+        }
+
+
+
+    }
+
+    
+    
 
     return 0;
 
