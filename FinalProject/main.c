35a51515abd6a2668f1964caa2779a86061ecfdb
wkeitz <wrkeitz@gmail.com>
2023-11-27 08:43:28 -0800

    another one

---
+++ FinalProject/main.c
@@ -0,0 +1,23 @@
+/*
+Name: Nathan Jaggers and Weston Keitz  
+
+Assignment Number: Final Project
+
+Description: Knock over cans, stay inside the square.
+Six cans in two minutes. Knock the other robot out of
+the area as well. 
+
+*/
+#include "globals.h"
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "functions.h"
+
+int main(){
+
+    int range = 
+
+    return 0;
+
+}
